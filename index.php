7b0d526973bab089d35aa76ce1a74b953a3d17d7
anish <anish@anish-acer>
2012-07-29 04:44:02 +0530

    basic ui added

--- index.php
+++ index.php
@@ -2,19 +2,59 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
-<title>Insert title here</title>
+<title>QuikFlic</title>
+<link rel="stylesheet" href="/quikflic/css/bootstrap.css">
 </head>
-<frameset>
-    <frame>
-    <frame>
-    <noframes>
-    <body>
-    <p>This page uses frames. The current browser you are using does not support frames.</p>
-    <?php
-    echo "hi";
 
-	?>
-    </body>
-    </noframes>
-</frameset>
+<body>
+	<div class="container-fluid">
+		<div class="navbar">
+  			<div class="navbar-inner">
+    			<div class="container">
+    				<a class="brand" href="#">
+  						QuikFlic
+					</a>
+    			</div>
+  			</div>
+		</div>
+  		<div class="row-fluid">
+  		
+    		<div class="span2">
+      			<!--Sidebar content-->
+      			<ul class="nav nav-list">
+      				<li class="nav-header">
+      					General
+      				</li>
+  					<li>
+    					<a href="#">
+      					<i class="icon-home"></i>
+     	 					Home
+    					</a>
+  					</li>
+  					<li>
+    					<a href="#">
+      					<i class="icon-file"></i>
+     	 					Files
+    					</a>
+  					</li>
+  					<li>
+    					<a href="#">
+      					<i class="icon-envelope"></i>
+     	 					Mails(0)
+    					</a>
+  					</li>
+				</ul>
+    		</div>
+    		<div class="span10">
+      			<!--Body content-->
+      			<form class="well">
+  					<label>Movie Name</label>
+  					<input type="text" class="span3">
+  					<span class="help-block">Type the name of the movie.</span>
+  					<button type="submit" class="btn">Submit</button>
+				</form>
+    		</div>
+  		</div>
+	</div>
+</body>
 </html>
